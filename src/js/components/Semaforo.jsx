2522cd5513116c11ce355594724a5c095509f451
jsvasquez356 <157722438+jsvasquez356@users.noreply.github.com>
2025-10-25 02:50:34 +0000

    comentario2

--- src/js/components/Semaforo.jsx
+++ src/js/components/Semaforo.jsx
@@ -1,41 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import './Semaforocss.css';
+import "/styles/semaforocss.css";
 
 const Semaforocss = () => {
-  const [luzActiva, setLuzActiva] = useState('rojo');
-  const [ClickEnLaLuz, setClickEnLaLuz] = useState(null);
+  const [luzActiva, setLuzActiva] = useState(null);
 
-  useEffect(() => {
-    const ciclo = ['rojo', 'amarillo', 'verde'];
-    let inicio = 0;
-
-    const intervalo = setInterval(() => {
-      inicio = (inicio + 1) % ciclo.length;
-      setLuzActiva(ciclo[inicio]);
-    }, 3000);
-
-    return () => clearInterval(intervalo);
-  }, []);
-
-  const ClickFoco = (color) => {
-    setClickEnLaLuz(color);
-    setTimeout(() => setClickEnLaLuz(null), 3000);
-  };
-
-  const LuzBrillante = (color) => {
-    const activa = luzActiva === color ? color : 'apagado';
-    const brillo = ClickEnLaLuz === color ? 'brillo' : '';
-    return `luz ${activa} ${brillo}`;
+  const luzEncendida = (color) => {
+    return `luz ${luzActiva === color ? color : 'apagado'}`;
   };
 
   return (
-    <div className="Semaforo mx-auto mt-5">
-      <div className={LuzBrillante('rojo')} onClick={() => ClickFoco('rojo')}></div>
-      <div className={LuzBrillante('amarillo')} onClick={() => ClickFoco('amarillo')}></div>
-      <div className={LuzBrillante('verde')} onClick={() => ClickFoco('verde')}></div>
+    <div className="semaforo mx-auto mt-5">
+      <div className={luzEncendida('rojo')} onClick={() => setLuzActiva('rojo')}></div>
+      <div className={luzEncendida('amarillo')} onClick={() => setLuzActiva('amarillo')}></div>
+      <div className={luzEncendida('verde')} onClick={() => setLuzActiva('verde')}></div>
     </div>
   );
 };
-
 
 export default Semaforocss;
